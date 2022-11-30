f281cfb56349ffe5bea80860bb76544c13b6d702
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-11-30 12:15:46 +0600

    discussed about the method of creating object

---
+++ explore.js
@@ -0,0 +1,50 @@
+// 1. creat object using object literal:
+const player = {};
+player.name = 'small Nirob';
+player.specialty = 'batsman';
+player.bat = function () {
+    console.log('swing your bat');
+}
+// console.log(player);
+// player.bat();
+
+const student = {
+    name: 'pandey',
+    job: 'khay andey',
+    ball: function () {
+        console.log('throw the ball');
+    },
+    salary: 100000
+}
+
+// console.log(student);
+// student.ball();
+
+// 2. object constructor:
+const person = new Object();
+// console.log(person);
+
+// 3. object create method:
+// const item = Object.create(null);
+const atel = Object.create(student);
+// console.log(atel.friend);
+
+// 4. class:
+class Person {
+    name = 'Abul';
+    address = 'shodor ghat';
+    constructor(age) {
+        this.age = age;
+    }
+}
+
+const person1 = new Person(15);
+// console.log(person1);
+
+// 5. function: 
+function Car(model, price) {
+    this.model = model;
+    this.price = price;
+}
+const tesla = new Car('elon', 32);
+console.log(tesla);
