031318c426dd447b367cfa8044a319f17dd5a9ff
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-12-01 10:15:29 +0600

    discussed about values of object

--- freeze.js
+++ freeze.js
@@ -1,3 +1,6 @@
 const bottle = { color: 'yellow', price: 50, isCleaned: true, capacity: 1 };
 const keys = Object.keys(bottle);
-console.log(keys);
+// console.log(keys);
+
+const values = Object.values(bottle);
+// console.log(values);
