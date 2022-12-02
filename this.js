a4f5899ff11971cbe940ecfa153d13da135f46c2
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-12-02 22:55:06 +0600

    discussed about this keyword

---
+++ this.js
@@ -0,0 +1,51 @@
+// console.log(this);
+
+const kodomAli = {
+    name: 'Kodom Ali',
+    money: 5000,
+    study: 'Math',
+    subjects: ['Algebra', 'Calculas', 'Geometry'],
+    exam: function () {
+        console.log(this);
+        return (`${this.name} is participating in an exam.`);
+    },
+    examArrow: () => {
+        console.log(this);
+    },
+    examNested: () => {
+        const arrow = () => {
+            console.log(this);
+        }
+        arrow();
+    },
+    improveExam: function (subject) {
+        return (`${this.name} is taking an improve exam on ${subject}`);
+    },
+    treatDey: function (amount, tips) {
+        this.money = this.money - amount - tips;
+        return this.money;
+    }
+};
+
+kodomAli.exam();
+
+const badamAli = {
+    name: 'Kacha Badam Ali',
+    money: 8000
+};
+
+badamAli.exam = kodomAli.exam;
+badamAli.exam();
+
+function clickHandler() {
+    console.log('inside the click me', this);
+};
+
+document.getElementById('btn-click2').addEventListener('click', function () {
+    console.log(this);
+})
+
+function mine() {
+    console.log(this);
+}
+mine();
